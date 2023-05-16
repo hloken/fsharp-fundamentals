a0125b967b29f676acdcf42355b97488128c36c3
Hans Løken <hans.loken@elaway.io>
2023-05-16 11:05:44 +0200

    Added PrintAndPrintFn

---
+++ src/PrintAndPrintFn/Program.fs
@@ -0,0 +1,13 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+// printf
+let a = [|1; 2; 3;|]
+printfn "%A" a
+
+// Using System.Console
+System.Console.WriteLine(a)
+
+//
+printf "Enter your command human:"
+let input = System.Console.ReadLine()  // read input from console
+printfn "Your command is: %A" input
